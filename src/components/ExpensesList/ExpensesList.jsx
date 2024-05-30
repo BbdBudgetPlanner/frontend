7b7081e947a2d3f44e2a7c4fb89a31a25fe74208
ShaariqBBD <shaariq.patel@bbd.co.za>
2024-05-30 13:48:13 +0200

    Added Card to display a user's budget

--- src/components/ExpensesList/ExpensesList.jsx
+++ src/components/ExpensesList/ExpensesList.jsx
@@ -1,33 +1,26 @@
 import "./ExpensesList.css"
 
-const ExpensesList = () => {
-    const testArray = [
-        { name: "Coffee", price: 125.00, category: "Food", date: "21/05/2024" },
-        { name: "Biscuits", price: 45.00, category: "Food", date: "22/05/2024" },
-        { name: "Sugar", price: 60.00, category: "Food", date: "22/05/2024" },
-        { name: "Tea", price: 35.00, category: "Food", date: "23/05/2024" },
-        { name: "Milk", price: 25.00, category: "Food", date: "23/05/2024" },
-    ]
+const ExpensesList = ({ expenseList }) => {
 
     return (
         <div className="expenses-container">
             <div className="expenses-list">
                 <div className="list-row">
                     <div className="list-item" >
-                        <h2>Name</h2>
+                        <h3>Name</h3>
                     </div>
                     <div className="list-item">
-                        <h2>Price</h2>
+                        <h3>Price</h3>
                     </div>
                     <div className="list-item">
-                        <h2>Category</h2>
+                        <h3>Category</h3>
                     </div>
                     <div className="list-item">
-                        <h2>Date</h2>
+                        <h3>Date</h3>
                     </div>
                 </div>
                 {
-                    testArray.map((expense, index) => (
+                    expenseList.map((expense, index) => (
                         <div className="list-row" key={index}>
                             <div className="list-item">{expense.name}</div>
                             <div className="list-item">{expense.price}</div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,10 +4,19 @@ import "./Home.css"
 
 import CreateBudgetForm from "../../components/CreateBudgetForm/CreateBudgetForm.jsx";
 import AddExpenseForm from "../../components/AddExpenseForm/AddExpenseForm.jsx";
+import BudgetCard from "../../components/BudgetCard/BudgetCard.jsx";
 import ExpensesList from "../../components/ExpensesList/ExpensesList.jsx";
 
 const Home = () => {
     const navigate = useNavigate();
+
+    const testArray = [
+        { name: "Coffee", price: 125.00, category: "Food", date: "21/05/2024" },
+        { name: "Biscuits", price: 45.00, category: "Food", date: "22/05/2024" },
+        { name: "Sugar", price: 60.00, category: "Food", date: "22/05/2024" },
+        { name: "Tea", price: 35.00, category: "Food", date: "23/05/2024" },
+        { name: "Milk", price: 25.00, category: "Food", date: "23/05/2024" },
+    ]
 
     return (
         <div>
@@ -15,7 +24,8 @@ const Home = () => {
             <CreateBudgetForm />
             <AddExpenseForm />
             </div>
-            <ExpensesList />
+            <BudgetCard name={"Groceries"} total={1000.00} spent={750.00}/>
+            <ExpensesList expenseList={testArray}/>
             <button onClick={() => {
                 navigate("/login")
             }}>Login Page
