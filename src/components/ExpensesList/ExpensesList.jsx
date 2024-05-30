8123f3d4d18daeb9cc6d8d5a4f56aa93fa010ff7
ShaariqBBD <shaariq.patel@bbd.co.za>
2024-05-30 11:05:20 +0200

    Updated font, added table component to display expenses

--- src/components/ExpensesList/ExpensesList.jsx
+++ src/components/ExpensesList/ExpensesList.jsx
@@ -2,25 +2,41 @@ import "./ExpensesList.css"
 
 const ExpensesList = () => {
     const testArray = [
-        {name: "Coffee", price: 125.00, date: "21/05/2024"},
-        {name: "Biscuits", price: 45.00, date: "22/05/2024"},
-        {name: "Sugar", price: 60.00, date: "22/05/2024"},
-        {name: "Tea", price: 35.00, date: "23/05/2024"},
-        {name: "Milk", price: 25.00, date: "23/05/2024"},
+        { name: "Coffee", price: 125.00, category: "Food", date: "21/05/2024" },
+        { name: "Biscuits", price: 45.00, category: "Food", date: "22/05/2024" },
+        { name: "Sugar", price: 60.00, category: "Food", date: "22/05/2024" },
+        { name: "Tea", price: 35.00, category: "Food", date: "23/05/2024" },
+        { name: "Milk", price: 25.00, category: "Food", date: "23/05/2024" },
     ]
 
     return (
-        <div>
-            <h2>Expenses List:</h2>
-            {
-                testArray.map((expense, index) => (
-                    <div className="list-row" key={index}>
-                        <div>{expense.name}</div>
-                        <div>{expense.price}</div>
-                        <div>{expense.date}</div>
+        <div className="expenses-container">
+            <div className="expenses-list">
+                <div className="list-row">
+                    <div className="list-item" >
+                        <h2>Name</h2>
                     </div>
-                ))
-            }
+                    <div className="list-item">
+                        <h2>Price</h2>
+                    </div>
+                    <div className="list-item">
+                        <h2>Category</h2>
+                    </div>
+                    <div className="list-item">
+                        <h2>Date</h2>
+                    </div>
+                </div>
+                {
+                    testArray.map((expense, index) => (
+                        <div className="list-row" key={index}>
+                            <div className="list-item">{expense.name}</div>
+                            <div className="list-item">{expense.price}</div>
+                            <div className="list-item">{expense.category}</div>
+                            <div className="list-item">{expense.date}</div>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
