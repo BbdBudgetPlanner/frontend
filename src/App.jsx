a4e0103603b1512618085f5ea993f99e33a21a32
Derryk Fivaz <Derryk.Fivaz@bbd.co.za>
2024-06-01 21:11:40 +0200

    add confirmation page

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,59 @@
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Main, { mainLoader } from "./layouts/Main/Main.jsx";
 import Home, { homeLoader } from "./pages/Home/Home.jsx";
 import Budget, { budgetLoader } from "./pages/Budget/Budget.jsx";
-import Login, { loginLoader } from "./pages/Login/Login.jsx"
+import Login, { loginLoader } from "./pages/Login/Login.jsx";
 import SignUp from "./pages/SignUp/SignUp.jsx";
 import Error from "./pages/Error/Error.jsx";
+import Confirmation from "./pages/Confirmation/Confirmation.jsx";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Main />,
-        loader: mainLoader,
+  {
+    path: "/",
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+        loader: homeLoader,
         errorElement: <Error />,
-        children: [
-            {
-                index: true,
-                element: <Home />,
-                loader: homeLoader,
-                errorElement: <Error />,
-            },
-            {
-                path: "budget/:id",
-                element: <Budget />,
-                loader: budgetLoader,
-                errorElement: <Error />,
-            },
-        ]
-    },
-    {
-        path: "login",
-        index: true,
-        element: <Login />,
-        loader: loginLoader,
-    },
-    {
-        path: "signup",
-        index: true,
-        element: <SignUp />,
-    }
+      },
+      {
+        path: "budget/:id",
+        element: <Budget />,
+        loader: budgetLoader,
+        errorElement: <Error />,
+      },
+    ],
+  },
+  {
+    path: "login",
+    index: true,
+    element: <Login />,
+    loader: loginLoader,
+  },
+
+  {
+    path: "confirmation",
+    index: true,
+    element: <Confirmation />,
+  },
+  {
+    path: "signup",
+    index: true,
+    element: <SignUp />,
+  },
 ]);
 
 function App() {
-
   return (
-      <div className="App">
-          <RouterProvider router={router} />
-      </div>
-  )
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App
+export default App;
