cb737079a1a3ae0b8ff8d6d11974f4904be34944
Derryk Fivaz <Derryk.Fivaz@bbd.co.za>
2024-06-01 12:20:45 +0200

    added auth which generates a valid jwt token for backend

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,39 +1,69 @@
-import {useState} from "react";
-
-import "./LoginForm.css"
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { Amplify } from "aws-amplify";
+import "./LoginForm.css";
+import { useNavigate } from "react-router-dom";
+import { signIn } from "aws-amplify/auth";
+import { getCurrentUser } from "aws-amplify/auth";
+import { fetchAuthSession } from "aws-amplify/auth";
 
 const LoginForm = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    return (
-        <div className="login-container">
-            <h2 className="login-header">Login</h2>
-            <form className="login-form" onSubmit={(e) => e.preventDefault()}>
-                <input
-                    className="login-input"
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <input
-                    className="login-input"
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-                <button className="login-button" type="submit">Login</button>
-                <button className="sign-up-route" onClick={() => navigate("/signup")}>Don't have an account? Sign Up</button>
-            </form>
-        </div>
-    )
-}
+    try {
+      await signIn({
+        username: email,
+        password: password,
+      });
+      const session = await fetchAuthSession();
+      //This is jwt
+      console.log(session.tokens.accessToken.toString());
+    } catch (error) {
+      console.error("Error signing in:", error);
+      setError(
+        "Failed to sign in. Please check your credentials and try again."
+      );
+    }
+  };
 
-export default LoginForm
+  return (
+    <div className="login-container">
+      <h2 className="login-header">Login</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <input
+          className="login-input"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          className="login-input"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        {error && <p className="error-message">{error}</p>}
+        <button className="login-button" type="submit">
+          Login
+        </button>
+        <button
+          className="sign-up-route"
+          type="button"
+          onClick={() => navigate("/signup")}
+        >
+          Don't have an account? Sign Up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
