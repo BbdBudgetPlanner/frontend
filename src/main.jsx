add8a5ee32da09193eb6d9d195e3fdc54995ed68
Derryk Fivaz <Derryk.Fivaz@bbd.co.za>
2024-06-04 15:47:56 +0200

    remove loggin

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import awsConfig from "../aws-config.js";
-console.log(awsConfig);
 Amplify.configure(awsConfig);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
